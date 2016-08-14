cd29d8eccd2a8c061f83ab29e4c9ca73ab29621c
Shae Kuronen <shae.kuronen@gmail.com>
2016-08-14 15:30:31 -0700

    added example of how to handle variable size picture element + picturefull

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -2,9 +2,7 @@
 
 // Add tests: https://www.npmjs.com/package/browsernizr
 import 'browsernizr/test/css/rgba';
-
 import Modernizr from 'browsernizr';
-
 import $ from 'jquery';
-
 import 'eqcss';
+import 'picturefill';

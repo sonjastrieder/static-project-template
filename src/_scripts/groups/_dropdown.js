3e5ccdd1cbfb8682d5069e12e7cd91fc14eb7914
Ray Knight <ray.knight@fellswoop.com>
2016-11-07 14:22:22 -0800

    Prevented clicks within dropdowns closing menu, clean up

--- src/_scripts/groups/_dropdown.js
+++ src/_scripts/groups/_dropdown.js
@@ -2,3 +2,16 @@
 
 import {prefix} from '../utilities/_helpers';
 
+let $dropdowns = window.$(`.${prefix('dropdown')}`);
+
+if (!!$dropdowns.length) {
+    let prevent = false;
+
+    $dropdowns.on('hide.bs.dropdown', (e) => {
+        (prevent && e.preventDefault());
+
+        prevent = false;
+    });
+
+    $dropdowns.on('click', '.dropdown-menu', () => prevent = true);
+}

2fd323bd141b79623e23ff4ecfb02b8c286bc4fd
Ray Knight <ray.knight@fellswoop.com>
2016-11-08 14:57:50 -0800

    Clean up, consistency, carousel arrows

--- src/_scripts/components/_carousel.js
+++ src/_scripts/components/_carousel.js
@@ -14,27 +14,7 @@ if (!!$carousels.length) {
         'mobileFirst': true,
         'variableWidth': true,
         'slidesToShow': 3,
-        'swipeToSlide': true,
-        'prevArrow': `
-            <button type="button" class="slick-prev">
-                <i class="${prefix('icon', 'icon-caret-thin-left')}">
-                    <svg>
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-caret-thin-left"></use>
-                    </svg>
-                </i>
-                <span class="sr-only">Previous</span>
-            </button>
-        `,
-        'nextArrow': `
-            <button type="button" class="slick-next">
-                <i class="${prefix('icon', 'icon-caret-thin-right')}">
-                    <svg>
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-caret-thin-right"></use>
-                    </svg>
-                </i>
-                <span class="sr-only">Next</span>
-            </button>
-        `
+        'swipeToSlide': true
     };
 
     $carousels.each((index) => {
